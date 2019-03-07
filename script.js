3441bf057cc2a42ea280ccefe090120bc3f59dcd
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2019-03-07 17:47:52 +0100

    add basic reset function

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 const container = document.querySelector("#container");
+const resetButton = document.querySelector('#reset');
 
 
 function createGrid(){
     for(let i = 0; i < (16*16); i++){
-        const square = document.createElement('div');
+        let square = document.createElement('div');
         square.classList.add('squares');
         container.appendChild(square);
         
@@ -15,4 +16,15 @@ function createGrid(){
 }
 
 createGrid();
+
+let squares = document.querySelectorAll('.squares');
+
+resetButton.addEventListener('click', function(){
+    for(i = 0; i < squares.length; i++){
+        squares[i].classList.remove('hovered');
+    }
+});
+
+
+
 
