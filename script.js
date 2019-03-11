8945b71b81a2c1ab08fd2726317fb4e0abf7e4f9
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2019-03-11 16:02:10 +0100

    mouseover now creates RANDOM background colors

--- script.js
+++ script.js
@@ -2,29 +2,37 @@
 const container = document.querySelector("#container");
 const resetButton = document.querySelector('#reset');
 
+// this function generates ONE random color
+function generateRandomColor(){
+    let rValue = (Math.floor(Math.random()*255));
+    let gValue = (Math.floor(Math.random()*255));
+    let bValue = (Math.floor(Math.random()*255));
+    
+    return  'rgb(' + rValue + ', ' + gValue + ', ' + bValue + ')';
+}
 
+// this function creates the square grid
 function createGrid(){
     for(let i = 0; i < (16*16); i++){
         let square = document.createElement('div');
         square.classList.add('squares');
         container.appendChild(square);
+
+        let randomColor = generateRandomColor();
         
         square.addEventListener('mouseover', function(){
-            square.classList.add('hovered');
+            square.style.backgroundColor = randomColor;
         })
     }
 }
 
 createGrid();
 
-let squares = document.querySelectorAll('.squares');
 
+let squares = document.querySelectorAll('.squares');
 resetButton.addEventListener('click', function(){
     for(i = 0; i < squares.length; i++){
-        squares[i].classList.remove('hovered');
+        squares[i].style.backgroundColor = "transparent";
     }
 });
-
-
-
 
