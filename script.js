d86e342b4d3d791489ed07446e9545508511ea5b
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2019-03-12 14:27:19 +0100

    add basic styling and change default grid to 48x48

--- script.js
+++ script.js
@@ -1,54 +1,70 @@
 
 const container = document.querySelector("#container");
 const resetButton = document.querySelector('#reset');
-let squaresPerSide;
 
 
-// this function generates ONE random color
-function generateRandomColor(){
-    let rValue = (Math.floor(Math.random()*255));
-    let gValue = (Math.floor(Math.random()*255));
-    let bValue = (Math.floor(Math.random()*255));
-    
-    return  'rgb(' + rValue + ', ' + gValue + ', ' + bValue + ')';
-}
+createFirstGrid();
 
-// this function creates the square grid
-function createGrid(){
+// the reset button removes all divs inside container and calls the createNewGrid function
+resetButton.addEventListener('click', function () {
+    let allSquares = document.querySelectorAll(".squares");
+    console.log(allSquares);
+    console.log(allSquares[0]);
+    for (i = 0; i < allSquares.length; i++) {
+        allSquares[i].style.backgroundColor = "transparent";
+        container.removeChild(allSquares[i]);
+    }
+    createNewGrid();
+});
 
-    squaresPerSide = Number(prompt("How many squares per side?"));
 
-    container.style.gridTemplateColumns = 'repeat(' + squaresPerSide + ', 1fr)';
-    container.style.gridTemplateRows = 'repeat(' + squaresPerSide + ', 1fr)';
-
-    for(let i = 0; i < (squaresPerSide*squaresPerSide); i++){
+// this function creates the initial 16x16 grid
+function createFirstGrid() {
+    for (let i = 0; i < (48 * 48); i++) {
         let square = document.createElement('div');
         square.classList.add('squares');
-        square.style.width = '"' + (540/squaresPerSide) + 'px' + '"';
-        square.style.height = '"' + (540/squaresPerSide) + 'px' + '"';
+        square.style.width = '"' + (540 / 48) + 'px' + '"';
+        square.style.height = '"' + (540 / 48) + 'px' + '"';
         container.appendChild(square);
 
         let randomColor = generateRandomColor();
-        
-        square.addEventListener('mouseover', function(){
+
+        square.addEventListener('mouseover', function () {
             square.style.backgroundColor = randomColor;
         })
     }
 }
 
 
+// this function creates a new grid with the prompted number of squares
+function createNewGrid() {
 
-createGrid();
+    let squaresPerSide = Number(prompt("How many squares per side?"));
 
+    container.style.gridTemplateColumns = 'repeat(' + squaresPerSide + ', 1fr)';
+    container.style.gridTemplateRows = 'repeat(' + squaresPerSide + ', 1fr)';
 
-resetButton.addEventListener('click', function(){
-    let allSquares = document.querySelectorAll(".squares");
-    console.log(allSquares);
-    console.log(allSquares[0]);
-    for(i = 0; i < allSquares.length; i++){
-        // squares[i].style.backgroundColor = "transparent";
-        container.removeChild(allSquares[i]);
+    for (let i = 0; i < (squaresPerSide * squaresPerSide); i++) {
+        let square = document.createElement('div');
+        square.classList.add('squares');
+        square.style.width = '"' + (540 / squaresPerSide) + 'px' + '"';
+        square.style.height = '"' + (540 / squaresPerSide) + 'px' + '"';
+        container.appendChild(square);
+
+        let randomColor = generateRandomColor();
+
+        square.addEventListener('mouseover', function () {
+            square.style.backgroundColor = randomColor;
+        })
     }
-    createGrid();
-});
+}
 
+
+// this function generates ONE random color
+function generateRandomColor() {
+    let rValue = (Math.floor(Math.random() * 255));
+    let gValue = (Math.floor(Math.random() * 255));
+    let bValue = (Math.floor(Math.random() * 255));
+
+    return 'rgb(' + rValue + ', ' + gValue + ', ' + bValue + ')';
+}
